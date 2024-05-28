42700f94ddec0779e4c8d7ba2c67aecdd07a2b5e
Gabriel Machado <gabriel@courtyard.io>
2024-05-28 12:16:16 -0300

    add copilot config

--- lua/community.lua
+++ lua/community.lua
@@ -7,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
